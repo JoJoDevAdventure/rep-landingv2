6925cb3f595bbedc340582f55db85a57a3c4a170
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2025-02-10 02:42:16 +0100

    +responsive Inudstries

--- src/Components/IndustriesMobile.jsx
+++ src/Components/IndustriesMobile.jsx
@@ -21,11 +21,11 @@ const industries = [
     translateX2: "-translate-x-1",
     translateY2: "translate-y-6",
 
-    scaleX1:"scale-x-125",
-    scaleY1:"scale-y-100",
+    scaleX1: "scale-x-125",
+    scaleY1: "scale-y-100",
 
-    scaleX2:"scale-x-100",
-    scaleY2:"scale-y-75",
+    scaleX2: "scale-x-100",
+    scaleY2: "scale-y-75",
   },
   {
     name: "Real-Estate",
@@ -44,11 +44,11 @@ const industries = [
     translateX2: "-translate-x-0",
     translateY2: "-translate-y-20",
 
-    scaleX1:"scale-x-25",
-    scaleY1:"scale-y-100",
+    scaleX1: "scale-x-25",
+    scaleY1: "scale-y-100",
 
-    scaleX2:"scale-x-100",
-    scaleY2:"scale-y-100",
+    scaleX2: "scale-x-100",
+    scaleY2: "scale-y-100",
   },
   {
     name: "Ecommerce",
@@ -67,11 +67,11 @@ const industries = [
     translateX2: "-translate-x-28",
     translateY2: "-translate-y-32",
 
-    scaleX1:"scale-x-150",
-    scaleY1:"scale-y-100",
+    scaleX1: "scale-x-150",
+    scaleY1: "scale-y-100",
 
-    scaleX2:"scale-x-100",
-    scaleY2:"scale-y-50",
+    scaleX2: "scale-x-100",
+    scaleY2: "scale-y-50",
   },
   {
     name: "Utility",
@@ -90,11 +90,11 @@ const industries = [
     translateX2: "-translate-x-44",
     translateY2: "-translate-y-22",
 
-    scaleX1:"scale-x-25",
-    scaleY1:"scale-y-100",
+    scaleX1: "scale-x-25",
+    scaleY1: "scale-y-100",
 
-    scaleX2:"scale-x-50",
-    scaleY2:"scale-y-25",
+    scaleX2: "scale-x-50",
+    scaleY2: "scale-y-25",
   },
   {
     name: "Funding",
@@ -110,15 +110,23 @@ const industries = [
     translateY1: "-translate-y-5",
     translateX2: "translate-x-2",
     translateY2: "translate-y-2",
-    scaleX1:"scale-x-2",
-    scaleX2:"scale-x-2",
-    scaleY1:"scale-y-2",
-    scaleY2:"scale-y-2",
+    scaleX1: "scale-x-2",
+    scaleX2: "scale-x-2",
+    scaleY1: "scale-y-2",
+    scaleY2: "scale-y-2",
   },
 ];
 
 const IndustriesSectionMobile = () => {
   const [selectedIndustry, setSelectedIndustry] = useState(industries[0]);
+
+  const handleIndustryClick = (industry, index) => {
+    setSelectedIndustry(industry);
+    document.getElementById(`industry-${index}`).scrollIntoView({
+      behavior: "smooth",
+      inline: "center",
+    });
+  };
 
   return (
     <section className="w-full py-8 flex flex-col items-center px-6 md:px-16 overflow-hidden">
@@ -126,12 +134,14 @@ const IndustriesSectionMobile = () => {
         Your Industry, Our Expertise
       </h1>
 
-      <div className="flex flex-row justify-start gap-4 mt-8 w-screen overflow-x-scroll py-2">
+      <div className="flex flex-row justify-start gap-4 mt-8 w-screen overflow-x-scroll snap-x snap-mandatory py-2 scrollbar-hide">
         {industries.map((industry) => (
           <button
             key={industry.name}
             onClick={() => setSelectedIndustry(industry)}
-            className={`px-6 py-3 rounded-full transition-all duration-300 text-[12px] ${industry.name == "Real-Estate" ? "min-w-32" : ""} ${
+            className={`first:ml-4 last:mr-4 px-6 py-3 rounded-full transition-all duration-300 text-[12px] snap-start ${
+              industry.name == "Real-Estate" ? "min-w-32" : ""
+            } ${
               selectedIndustry.name === industry.name
                 ? "border border-orange-500 text-orange-500"
                 : "bg-gray-200 text-gray-600"
@@ -155,7 +165,10 @@ const IndustriesSectionMobile = () => {
             {selectedIndustry.title}
           </h2>
           <p className="text-gray-700 mt-4">{selectedIndustry.description}</p>
-          <a href="#" className={`${selectedIndustry.textColor} text-purple-500 mt-4 inline-block`}>
+          <a
+            href="#"
+            className={`${selectedIndustry.textColor} text-purple-500 mt-4 inline-block`}
+          >
             {selectedIndustry.link}
           </a>
         </motion.div>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -89,5 +89,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwind-scrollbar-hide")],
 };

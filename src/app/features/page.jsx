4f2b0b8359688e2cd1ebc55dd1089a71c03de585
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2025-02-24 19:45:47 +0100

    update navbar

--- src/app/features/page.jsx
+++ src/app/features/page.jsx
@@ -3,9 +3,9 @@
 import dynamic from "next/dynamic";
 
 // Dynamically import components with `ssr: false` to disable server-side rendering
-const LiveTranscription = dynamic(() => import("@/Components/LiveTranscription"), { ssr: false });
-const ComparisonChart = dynamic(() => import("@/Components/Comparison"), { ssr: false });
-const NavBar = dynamic(() => import("@/Components/NavBar"), { ssr: false });
+const LiveTranscription = dynamic(() => import("./LiveTranscription"), { ssr: false });
+const ComparisonChart = dynamic(() => import("./Comparison"), { ssr: false });
+const NavBar = dynamic(() => import("./NavBar"), { ssr: false });
 
 const page = () => {
   return (

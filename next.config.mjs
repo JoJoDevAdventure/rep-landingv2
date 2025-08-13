7f475ff5b33d82d674e2fbefe0013aee3a97660a
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2025-08-13 18:28:07 +0100

    fix deployment 3

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
+import path from "path";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(process.cwd(), "src");
+    config.resolve.alias["@/components"] = path.resolve(process.cwd(), "src/components");
+    return config;
+  },
+};
 
 export default nextConfig;

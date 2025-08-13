1e3a8704c6e9b2e9f8cc151476be1f74465ec4b1
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2025-08-13 18:09:02 +0100

    + calculator fix 2

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Replicaide - Your AI-Powered Agent",
@@ -36,7 +25,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`antialiased`}
       >
         {children}
         <Analytics />
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,30 +1,30 @@
 "use client";
 
+import ContactPopup from "@/components/ContactPopup";
 import dynamic from "next/dynamic";
 import Script from "next/script";
 import { useState } from "react";
-import ContactPopup from "../components/ContactPopup";
 
 // Dynamically import components with `ssr: false` to disable server-side rendering
-const NavBar = dynamic(() => import("../components/NavBar"), { ssr: false });
-const Hero = dynamic(() => import("../components/Hero"), { ssr: false });
-const Partners = dynamic(() => import("../components/Partners"), { ssr: false });
-const Aides = dynamic(() => import("../components/Aides"), { ssr: false });
-const AIFuture = dynamic(() => import("../components/AIFuture"), { ssr: false });
-const IndustriesSection = dynamic(() => import("../components/Industries"), {
+const NavBar = dynamic(() => import("@/components/NavBar"), { ssr: false });
+const Hero = dynamic(() => import("@/components/Hero"), { ssr: false });
+const Partners = dynamic(() => import("@/components/Partners"), { ssr: false });
+const Aides = dynamic(() => import("@/components/Aides"), { ssr: false });
+const AIFuture = dynamic(() => import("@/components/AIFuture"), { ssr: false });
+const IndustriesSection = dynamic(() => import("@/components/Industries"), {
   ssr: false,
 });
 const IndustriesSectionMobile = dynamic(
-  () => import("../components/IndustriesMobile"),
+  () => import("@/components/IndustriesMobile"),
   { ssr: false }
 );
-const UnityAide = dynamic(() => import("../components/UnityAide"), {
+const UnityAide = dynamic(() => import("@/components/UnityAide"), {
   ssr: false,
 });
-const PerfectFit = dynamic(() => import("../components/PerfectFit"), {
+const PerfectFit = dynamic(() => import("@/components/PerfectFit"), {
   ssr: false,
 });
-const Footer = dynamic(() => import("../components/Footer"), { ssr: false });
+const Footer = dynamic(() => import("@/components/Footer"), { ssr: false });
 
 const Home = () => {
   const [isPopupOpen, setPopupOpen] = useState(false);
